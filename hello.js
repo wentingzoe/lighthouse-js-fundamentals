f09c1bd6725a06b9f1df919b7a8a1fdbd84e7413
wentingzoe <ywtzoe@gmail.com>
2021-11-01 23:25:59 -0400

    Add sayhello fuction

---
+++ hello.js
@@ -0,0 +1,12 @@
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
