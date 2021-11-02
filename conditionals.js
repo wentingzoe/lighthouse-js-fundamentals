bcc09f5063406da8f3af64589291bb805d21f158
wentingzoe <ywtzoe@gmail.com>
2021-11-01 21:52:42 -0400

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -9,3 +9,12 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+let raining = false;
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
