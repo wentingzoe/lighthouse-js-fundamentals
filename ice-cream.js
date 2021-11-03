a974bb988f0e84c701c6b55a5703574430ffab73
wentingzoe <ywtzoe@gmail.com>
2021-11-03 19:16:44 -0400

    Add ice-cream.js

---
+++ ice-cream.js
@@ -0,0 +1,16 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//Get the first flavour
+console.log(iceCreamFlavours[0]);
+
+//Get the Last Flavour
+console.log(iceCreamFlavours[6]);
+
+//Total Number of Flavours
+console.log(iceCreamFlavours.length);
